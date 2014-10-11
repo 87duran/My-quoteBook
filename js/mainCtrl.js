3edb0e9db60f5190cefef42861f7dca6d82a3a37
87duran <87duran@gmail.com>
2014-10-10 21:50:40 -0600

    latest commit

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -4,17 +4,28 @@ app.controller('mainCtrl', function($scope, dataService) {
 	$scope.quotes = dataService.getData();
 	$scope.toggleSearch = function(){
 		$scope.showSearch = !$scope.showSearch;
+		$scope.showSearch2 = false;
+		$scope.showSearch3 = false;
 	};
 	$scope.toggleSearch2 = function(){
 		$scope.showSearch2 = !$scope.showSearch2;
+		$scope.showSearch = false;
+		$scope.showSearch3 = false;
+	};
+	$scope.toggleSearch3 = function(){
+		$scope.showSearch3 = !$scope.showSearch3;
+		$scope.showSearch = false;
+		$scope.showSearch2 = false;
 	};
 	$scope.addQuote = function() {
 		dataService.addData($scope.quote);
 		$scope.quotes = dataService.getData();
+		$scope.quote = ''
 	};
 	$scope.removeQuote = function() {
-		dataService.removeData($scope.quote);
+		dataService.removeData($scope.removeQuote);
 		$scope.quotes = dataService.getData();
+		$scope.removeQuote = ''
 	};
 
 
